10871c129b7693fe013d58d34c20c5c74d9a8488
Sabiq Muhammad <sabiqmuhammad98@gmail.com>
2021-11-06 10:00:57 +0700

    tugas 7

---
+++ tugas-7-php-oop/Ape.php
@@ -0,0 +1,17 @@
+<?php
+
+  class Ape extends Animal 
+  {
+    public function get_legs()
+    {
+      return 2;
+    }
+
+    public function yell() 
+    {
+      return 'Auooo';
+    }
+  }
+
+
+?>
---
+++ tugas-7-php-oop/Frog.php
@@ -0,0 +1,12 @@
+<?php
+  // include('animal.php');
+
+  class Frog extends Animal 
+  {
+    public function jump() 
+    {
+      return 'hop hop';
+    }
+  }
+
+?>
